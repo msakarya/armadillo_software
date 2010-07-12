76929efc9801fda4277c4dc5687c386c4d72727a
Mustafa Sakarya <mustafasakarya1986@gmail.com>
2010-07-12 18:59:01 +0300

    NetBeans IDE Integration
    Base Project for PyGTK development

--- gui/main.py
+++ gui/main.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python
+import sys
+f=open("debug.txt","w")
+class STD:
+    def __init__(self):        
+        a=0
+    def write(self,arg=None):
+        f.write(arg)  
+        
+debug=False
+if debug:
+    sys.stderr=STD()
+    sys.stdout=sys.stderr
+
 import pygtk
 pygtk.require("2.0")
 import sys
 import gtk
 
 class Main:
-    def __init__(self):
-        builder = gtk.Builder()
+    def __init__(self):        
+        builder = gtk.Builder()                 
         builder.add_from_file("gui.glade")
         self.window = builder.get_object("window1")
-        builder.connect_signals(self)
-
+        self.about = builder.get_object("aboutdialog1")
+        builder.connect_signals(self)  
+        print("selam")
     def on_window1_destroy(self, widget, data=None):
         gtk.main_quit()
-    
+    def on_aboutdialog1_show(self, widget, data=None):
+        self.about.show()
+    def on_aboutdialog1_response(self, widget, data=None):        
+        self.about.hide()    
 
 if __name__=="__main__":
-    app=Main()
-    app.window.show()    
+    app=Main()   
+    app.window.show()
     gtk.main()
