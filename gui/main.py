d50ddfdbaaafa132e665d5b3606efb8038731a64
Mustafa Sakarya <mustafasakarya1986@gmail.com>
2010-07-13 17:39:16 +0300

    Preferences
    Will be stored in config.pickle

--- gui/main.py
+++ gui/main.py
@@ -1,22 +1,4 @@
 #!/usr/bin/env python
-import sys
-
-debug_file="debug.txt"
-glade_file="gui.glade"
-
-class STD:
-    def __init__(self,file):
-        self.file=file
-    def write(self,arg=None):
-        self.file.write(arg)
-        sys.__stdout__.write(arg)        
-        
-mod="normal"
-if mod=="debug":    
-    f=open(debug_file,"w")
-    sys.stdout=STD(f)
-    sys.stderr=sys.stdout
-
 import pygtk
 pygtk.require("2.0")
 import sys
@@ -27,10 +9,11 @@ from com import COM
 class Main:
     def __init__(self):        
         builder = gtk.Builder()                 
-        builder.add_from_file(glade_file)
+        builder.add_from_file(conf.d['glade_file'])
         self.window = builder.get_object("window1")
         self.about = builder.get_object("aboutdialog1")
         self.tbconnect=builder.get_object("tbconnect")
+        self.preferences_dialog=builder.get_object("preferences_dialog")
         builder.connect_signals(self)
 
         self.com=COM()
@@ -43,13 +26,57 @@ class Main:
             widget.set_label("Connected")
         else:                  
             self.com.disconnect()
-            widget.set_label("      Closed")       
+            widget.set_label("      Closed")
+    def on_preferences_dialog_show(self, widget, data=None):
+        for item in conf.d:
+            print(item,conf.d[item])
+        self.preferences_dialog.show()    
+    def on_bt_preferences_dialog_new_clicked(self, widget, data=None):
+        print(data)
+    def on_bt_preferences_dialog_save_clicked(self, widget, data=None):
+        self.preferences_dialog.hide()
+    def on_bt_preferences_dialog_cancel_clicked(self, widget, data=None):
+        self.preferences_dialog.hide()
     def on_window1_destroy(self, widget, data=None):
         gtk.main_quit()
     def on_aboutdialog1_show(self, widget, data=None):
         self.about.show()
     def on_aboutdialog1_response(self, widget, data=None):        
-        self.about.hide()    
+        self.about.hide()
+        
+class Preferences:
+    def __init__(self,parent=None):
+        self.parent=parent
+
+class STD:
+    def __init__(self,file):
+        self.file=file
+    def write(self,arg=None):
+        self.file.write(arg)
+        sys.__stdout__.write(arg)
+
+class Configure:
+    def __init__(self):
+        import pickle
+        self.p=pickle
+        self.load()
+        d=self.d
+        if d['mod']=="debug":
+            f=open(d['debug_file'],"w")
+            sys.stdout=STD(f)
+            sys.stderr=sys.stdout
+    def load(self):        
+        pickle=self.p
+        fpickle=open('config.pickle','r')
+        self.d=pickle.load(fpickle) # Dictionary
+        fpickle.close()
+    def save(self):
+        pickle=self.p
+        fpickle=open('config.pickle','w')
+        pickle.dump(self.d, fpickle) # Dictionary
+        fpickle.close()
+
+conf=Configure()
 
 if __name__=="__main__":
     app=Main()   
